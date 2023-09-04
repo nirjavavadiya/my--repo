38fdcc7502b6bb5aae5f7dc300ab84b881e4f893
nirjavavadiya <nirjavavadiya@gmail.com>
2023-09-04 18:07:40 +0530

    04/09/2023

---
+++ AREA.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h.
+main()
+{         int l,b,area;
+	  clrscr();
+	  printf("enter the value of l:");
+	  scanf("%d",&l);
+
+	  printf("enter the value of b:")
+	  scanf("%d",&b")
+	  area=l*b;
+	  printf("area of:%d",area);
+	  getch();
+ }
