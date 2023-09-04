471a834aa0e1daf6093b97db679e2b8486b741d9
nirjavavadiya <nirjavavadiya@gmail.com>
2023-09-04 18:09:19 +0530

    04/09/2023

---
+++ 2-1.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+main()
+{
+	int a;
+       long long int b;
+	char n;
+	printf("\nenter your age :");
+	scanf("%d",&a);
+	printf("enter your blood group :");
+	scanf(" %c",&n);
+	printf("enter your mobile number :");
+	scanf("%lld",&b);
+	printf("paras saliya\n");
+	printf("age:%d\nblood group :%c\nmobile number :%lld\n",a,n,b);
+
+}
